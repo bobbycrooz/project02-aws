a6a9711b7a2c927cbbf030c43ec945e108f3e4b7
bobbycrooz <idrisloove@gmail.com>
2022-12-17 19:02:43 +0100

    uhf

--- src/server.ts
+++ src/server.ts
@@ -12,38 +12,34 @@ import { filterImageFromURL, deleteLocalFiles } from "./util/util";
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
-  // GET /filteredimage?image_url={{URL}}
-  app.get("/filteredimage", async (req, res) => {
-    const { image_url } = req.query;
-    if (!image_url) return res.send("no image url provided").end();
-    try {
-      let filteredImagePath = await filterImageFromURL(image_url);
-      if (filteredImagePath) {
-        res.sendFile(filteredImagePath);
-        deleteLocalFiles([filteredImagePath]);
+  app.get(
+    "/filteredimage",
+    (req, res, next) => {
+      // authentication
+      const authorization = req.headers.authorization.split(" ");
+      const token = authorization[1];
+      if (!token) {
+        res.status(401).send("not authorized").end();
       }
 
-      return;
-    } catch (error) {
-      res.status(401).send({ error: error });
+      next();
+    },
+    async (req, res) => {
+      const { image_url } = req.query;
+      if (!image_url) return res.send("no image url provided").end();
+      try {
+        let filteredImagePath = await filterImageFromURL(image_url);
+        if (filteredImagePath) {
+          res.sendFile(filteredImagePath);
+          deleteLocalFiles([filteredImagePath]);
+        }
+
+        return;
+      } catch (error) {
+        res.status(401).send({ error: error });
+      }
     }
-  });
-  // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
-  // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
-
-  /**************************************************************************** */
-
-  //! END @TODO1
+  );
 
   // Root Endpoint
   // Displays a simple message to the user
